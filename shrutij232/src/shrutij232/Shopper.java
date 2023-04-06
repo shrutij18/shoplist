a0af035870d382b79fecf5965926d178991bf84d
bca <bca@Bcalab3-m02>
2023-04-06 13:09:23 +0530

    first commit

---
+++ shrutij232/src/shrutij232/Shopper.java
@@ -0,0 +1,21 @@
+package shrutij232;
+
+public class Shopper {
+	int num_items;
+	double unit_cost;
+	String item_name;
+	double total_cost;
+	Shopper(int num_items,double unit_cost, String item_name)
+	{
+		this.num_items= num_items;
+		this.unit_cost= unit_cost;
+		this.item_name= item_name;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
